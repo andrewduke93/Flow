d3d9ef44986b92e8137bea96c81c52160c1cc67f
andrewduke93 <40124646+andrewduke93@users.noreply.github.com>
2026-01-25 20:56:22 +0000

    Auto-unregister old service workers on load

--- Flow-main/index.tsx
+++ Flow-main/index.tsx
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+
+// Unregister any old service workers for this scope
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    regs.forEach(reg => {
+      if (reg.scope.includes('/Flow/')) {
+        reg.unregister();
+      }
+    });
+  });
+}
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
